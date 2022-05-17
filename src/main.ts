7c9013f9dc37c95801329dd84438bb603fb4a86a
namnv3899 <nam.nv3899@gmail.com>
2022-05-18 02:19:06 +0700

    chore: update swagger

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
-const port = 3000;
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { configs } from './configs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  const options = new DocumentBuilder()
+    .setTitle('API Documentation Manager Social Network')
+    .setDescription('Developed by Iris_VMO')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(configs.port, () => {
+    console.log(`Server is running on http://localhost:${configs.port}`);
   });
 }
 bootstrap();
